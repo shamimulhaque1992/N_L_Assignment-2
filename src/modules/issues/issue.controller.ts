7c995cfdf74702a118e47af31858df72d18a596a
shamimulhaquewebsure <shamimul@websure.digital>
2026-05-22 16:29:05 +0600

    Enhance issue management by adding reporter reference in database schema and implementing issue retrieval methods in the service layer

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -20,9 +20,49 @@ const createIssue = async (req: Request, res: Response) => {
     });
   }
 };
-const getIssue = async (req: Request, res: Response) => {};
-const getIssues = async (req: Request, res: Response) => {};
-const updateIssue = async (req: Request, res: Response) => {};
+const getIssue = async (req: Request, res: Response) => {
+  try {
+    const result = await issueService.getIssue(req.params.id);
+    sendResponse(res, {
+      status: 200,
+      success: true,
+      message: "Issue retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    sendResponse(res, {
+      status: 500,
+      success: false,
+      message: "Issue could not be retrieved",
+      error: (error as Error).message,
+    });
+  }
+};
+const getIssues = async (req: Request, res: Response) => {
+  try {
+    const result = await issueService.getIssues();
+    sendResponse(res, {
+      status: 200,
+      success: true,
+      message: "Issues retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    sendResponse(res, {
+      status: 500,
+      success: false,
+      message: "Issues could not be retrieved",
+      error: (error as Error).message,
+    });
+  }
+};
+const updateIssue = async (req: Request, res: Response) => {
+  try {
+    const result = await issueService.updateIssue(req);
+  } catch (error) {
+    
+  }
+};
 const deleteIssue = async (req: Request, res: Response) => {};
 
 export const issueController = {
--- src/modules/issues/issue.service.ts
+++ src/modules/issues/issue.service.ts
@@ -2,12 +2,20 @@ import config from "../../config";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { pool } from "../../db";
 import { Request } from "express";
+import { sendResponse } from "../../utility/sendResponse";
 
 type Issue = {
   title: string;
   description: string;
   type: "bug" | "feature_request";
   status?: "open" | "in_progress" | "resolved";
+};
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
 };
 
 const createIssue = async (payload: Request) => {
@@ -32,9 +40,80 @@ const createIssue = async (payload: Request) => {
     throw new Error(error.message || "Issue could not be created");
   }
 };
-const getIssue = (payload: any) => {};
-const getIssues = (payload: any) => {};
-const updateIssue = (payload: any) => {};
+const getIssue = async (payload: any) => {
+  try {
+    const result = await pool.query(`SELECT * FROM issues WHERE id=$1`, [
+      payload,
+    ]);
+    if (result.rows.length === 0) {
+      throw new Error("No issues found");
+    }
+
+    const { reporter_id } = result.rows[0];
+    const userData = await pool.query(
+      `SELECT id, role, name FROM users WHERE id=$1`,
+      [reporter_id],
+    );
+    delete result.rows[0].reporter_id;
+    const formattedResult = { ...result.rows[0], reporter: userData.rows[0] };
+    return formattedResult;
+  } catch (error: any) {
+    throw new Error(error.message || "Issue could not be retrieved");
+  }
+};
+const getIssues = async () => {
+  try {
+    const issues = (await pool.query(`SELECT * FROM issues`)).rows;
+    // Collect unique reporter ids
+    const reporterIds: string[] = [];
+    issues.forEach((issue) => {
+      if (!reporterIds.includes(issue.reporter_id)) {
+        reporterIds.push(issue.reporter_id);
+      }
+    });
+
+    // Fetch reporters
+    const users = (
+      await pool.query(
+        `SELECT id, name, email, role FROM users WHERE id = ANY($1)`,
+        [reporterIds],
+      )
+    ).rows;
+
+    // Build lookup object
+    const usersById: Record<string, User> = {};
+    users.forEach((user) => {
+      usersById[user.id] = user;
+    });
+
+    // Inject reporter into each issue
+    issues.forEach((issue) => {
+      issue.reporter = usersById[issue.reporter_id];
+      delete issue.reporter_id;
+    });
+
+    return issues;
+  } catch (error) {}
+};
+const updateIssue = (payload: Request) => {
+  try {
+    const { id } = payload.params;
+    const { authorization } = payload.headers;
+    const { title, description, type, status } = payload.body;
+
+    const decodedToken = jwt.verify(
+      authorization as string,
+      config.jwtAccessSecret as string,
+    ) as JwtPayload;
+    if (!authorization) {
+      throw new Error("Unauthorized");
+    }
+
+    if(decodedToken.role==="contributor"){
+      
+    }
+  } catch (error) {}
+};
 const deleteIssue = (payload: any) => {};
 
 export const issueService = {
