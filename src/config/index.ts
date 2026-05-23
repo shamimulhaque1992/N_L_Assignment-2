e2133c6f88cffa0b8cdc798d8f70eb05f45cc9a8
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2026-05-23 17:27:57 +0600

    env config changed

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,5 @@ const config = {
   port: process.env.PORT || 5000,
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
   jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-  env: process.env.NODE_ENV || "development",
 };
 export default config;
